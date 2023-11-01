f99631b2b9a0141926afe144a9554cc5b1acede9
Cloud User <ec2-user@ip-172-31-41-204.ec2.internal>
2023-11-01 04:15:50 +0000

    Atualizar o arquivo lab05_2a.c para ser forçado a utilizar 4 threads, criar o arquivo lab05_2a_2.c que utiliza apenas uma thread para comparacao

--- lab05_2a_2.c
+++ lab05_2a_2.c
@@ -9,17 +9,23 @@ int main() {
     double h = (b - a) / n;
     double integral = 0.0;
 
-    // Defina o número de threads que deseja usar (por exemplo, 4 threads)
-    omp_set_num_threads(4);
-
     clock_t start_time, end_time;
     start_time = clock(); // Registrar o tempo inicial
 
-    #pragma omp parallel for reduction(+:integral)
-    for (int i = 1; i < n; i++) {
-        double x = a + i * h;
-        double fx = x * x;  // Função a ser integrada (no exemplo, f(x) = x^2)
-        integral += fx;
+    #pragma omp parallel
+    {
+        #pragma omp for reduction(+:integral)
+        for (int i = 1; i < n; i++) {
+            double x = a + i * h;
+            double fx = x * x;  // Função a ser integrada (no exemplo, f(x) = x^2)
+            integral += fx;
+        }
+
+        #pragma omp single
+        {
+            int num_threads = omp_get_num_threads();
+            printf("Número de threads utilizadas: %d\n", num_threads);
+        }
     }
 
     integral = 0.5 * (integral + (a * a + b * b)) * h;
@@ -29,8 +35,8 @@ int main() {
 
     printf("Resultado da integral: %f\n", integral);
     printf("Tempo de execução: %f segundos\n", execution_time);
-    printf("Número de threads utilizadas: %d\n", omp_get_max_threads());
 
     return 0;
 }
+
 
